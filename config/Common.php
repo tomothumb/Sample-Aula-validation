5e8dd23b450140ab4f2fe55ac7a0b399dc2bedb3
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2016-01-18 00:15:44 +0900

    Using Filter

--- config/Common.php
+++ config/Common.php
@@ -4,6 +4,8 @@ namespace Aura\Framework_Project\_Config;
 use Aura\Di\Config;
 use Aura\Di\Container;
 
+use Aura\Filter\FilterFactory;
+use FOA\Filter_Input_Bundle\Filter;
 
 
 class Common extends Config
@@ -59,6 +61,8 @@ class Common extends Config
         $router->add('hello', '/')
             ->setValues(array('action' => 'hello'));
 
+        $router->add('send', '/send')
+            ->setValues(array('action' => 'send'));
     }
 
     public function modifyWebDispatcher($di)
@@ -71,5 +75,36 @@ class Common extends Config
             $response->content->set('Hello World!');
         });
 
+        $dispatcher->setObject('send', function () use ($di) {
+
+            $request = $di->get('aura/web-kernel:request');
+            $input = $request->post->get();
+
+            $response = $di->get('aura/web-kernel:response');
+            $response->content->setType('application/json');
+            $response_content = array();
+
+
+
+            $filter_factory = new FilterFactory();
+            $filter = $filter_factory->newInstance();
+            $filter->addSoftRule('first_name', $filter::IS, 'string');
+            $filter->addSoftRule('first_name', $filter::IS, 'strlenBetween', 6, 12);
+            $success = $filter->values($input);
+            if ( $success) {
+                $response_content["message"] = "User input is valid.";
+            }else{
+                $response_content["message"][] = "User input is not valid.";
+                $messages = $filter->getMessages();
+                $response_content["message"] = $messages;
+            }
+
+            $response_content["data"] = $input;
+            $response->content->set($response_content);
+            $json = json_encode($response->content->get());
+            $response->content->set($json);
+
+
+        });
     }
 }
